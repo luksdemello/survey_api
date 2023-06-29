b47d6d5901ce4d3be309edaaea81a8d6a195ddff
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-29 18:27:58 -0300

    test: ensure AddAccountUseCase returns an account on success

--- src/data/use_cases/add_account/AddAccount.spec.ts
+++ src/data/use_cases/add_account/AddAccount.spec.ts
@@ -106,4 +106,22 @@ describe('AddAccount UseCase', () => {
     const promise = sut.execute(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  it('should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.execute(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
