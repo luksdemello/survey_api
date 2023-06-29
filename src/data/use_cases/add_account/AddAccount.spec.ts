8f5a1b1e7f089d1d9d6f7e68a32645cdb0a07d81
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-29 18:20:03 -0300

    test: ensure AddAccountRepository throws if AddAccountRepository throws

--- src/data/use_cases/add_account/AddAccount.spec.ts
+++ src/data/use_cases/add_account/AddAccount.spec.ts
@@ -92,4 +92,18 @@ describe('AddAccount UseCase', () => {
       password: 'hashed_password'
     })
   })
+
+  it('should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'create').mockReturnValueOnce(new Promise((resolve, reject) => {
+      reject(new Error())
+    }))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.execute(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
