56904e8b214bb337d158be986b61524b2e976d48
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-29 18:18:13 -0300

    feat: ensure AddAccountUsecase calls AddAccountRepository with correct values

--- src/data/use_cases/add_account/AddAccount.ts
+++ src/data/use_cases/add_account/AddAccount.ts
@@ -1,15 +1,21 @@
 import {
+  type AddAccountRepository,
   type AccountModel,
   type AddAccount, type AddAccountModel,
   type Encrypter
 } from './AddAccountProtocols'
 
 export class AddAccountUseCase implements AddAccount {
-  constructor(private readonly encrypter: Encrypter) {}
+  constructor(
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
 
-  async execute(account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async execute(accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.create(Object.assign({}, accountData, { password: hashedPassword }))
 
-    return { ...account, id: '' }
+    return { ...accountData, id: '' }
   }
 }
--- src/data/use_cases/add_account/AddAccountProtocols.ts
+++ src/data/use_cases/add_account/AddAccountProtocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/models/Account'
 export * from '../../../domain/use_cases/AddAccount'
 export * from '../../protocols/Encrypter'
+export * from '../../protocols/AddAccountRepository'
