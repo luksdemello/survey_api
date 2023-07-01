003c0d91e47efd6e52f01140b55163555d421a3d
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-01 10:28:21 -0300

    feat: ensure BcryptAdapter call bcrypt with correct value

---
+++ src/infra/adapters/cryptography/BcryptAdapter.ts
@@ -0,0 +1,11 @@
+import { type Encrypter } from '../../../data/protocols/Encrypter'
+import { hash } from 'bcrypt'
+
+export class BcryptAdapter implements Encrypter {
+  constructor(private readonly salt: number) {}
+  async encrypt(value: string): Promise<string> {
+    const hashedValue = await hash(value, this.salt)
+
+    return hashedValue
+  }
+}
