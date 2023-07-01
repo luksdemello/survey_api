91def7e4112e57439e84cf43efc8a373d2f3efd0
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-01 10:34:16 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/adapters/cryptography/BcryptAdapter.spec.ts
+++ src/infra/adapters/cryptography/BcryptAdapter.spec.ts
@@ -16,6 +16,11 @@ const makeSut = (): SutTypes => {
     salt
   }
 }
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => { resolve('hash') })
+  }
+}))
 
 describe('Bcrypt Adapter', () => {
   it('should call bcrypt with correct value', async () => {
@@ -24,5 +29,12 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a hash on success', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hash')
   })
 })
