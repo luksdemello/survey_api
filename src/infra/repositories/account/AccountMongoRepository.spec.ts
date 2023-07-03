8440e58c832eb16bdf69c849bb374ca188dbc064
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-03 20:07:22 -0300

    test: ensure AccountMongoRepository return an account on success

---
+++ src/infra/repositories/account/AccountMongoRepository.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../../db/mongodb/mongodb'
+import { AccountMongoRepository } from './AccountMongoRepository'
+
+describe('Account Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017/mongo-teste')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.dropDatabase('mongo-teste')
+    await MongoHelper.disconnect()
+  })
+
+  it('should return an account con success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.create({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
