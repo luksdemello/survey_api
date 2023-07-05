fa1516cd73aa615ff847ae33840a2aecd8ff475b
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-05 07:59:58 -0300

    test: ensure LogControllerDecorator return the same result of controller

--- src/main/decorators/Log.spec.ts
+++ src/main/decorators/Log.spec.ts
@@ -47,4 +47,23 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toBeCalledWith(httpRequest)
   })
+
+  it('should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name'
+      }
+    })
+  })
 })
