8029ee431380fdb934dcb3cfe46f1d13d8353d3d
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-08 19:27:18 -0300

    feat: ensure SignInController throws if emailValidator throws

--- src/presentation/controllers/sign_in/SignIn.ts
+++ src/presentation/controllers/sign_in/SignIn.ts
@@ -7,21 +7,25 @@ export class SignInController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
+    try {
+      const requiredFields = ['email', 'password']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return HttpHelpers.badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return HttpHelpers.badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const { email } = httpRequest.body
+      const { email } = httpRequest.body
 
-    this.emailValidator.isValid(email)
+      this.emailValidator.isValid(email)
 
-    return {
-      body: '',
-      statusCode: 400
+      return {
+        body: '',
+        statusCode: 400
+      }
+    } catch (error) {
+      return HttpHelpers.serverError(error)
     }
   }
 }
