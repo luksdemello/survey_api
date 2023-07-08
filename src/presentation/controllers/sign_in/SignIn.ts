7b1f46cf91f166e2bc9944f69c9a30fbddcdd390
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-08 19:23:24 -0300

    feat: ensure SignInController call EmailValidator with correct email

--- src/presentation/controllers/sign_in/SignIn.ts
+++ src/presentation/controllers/sign_in/SignIn.ts
@@ -1,8 +1,11 @@
 import { MissingParamError } from '../../errors'
 import { HttpHelpers } from '../../helpers/HttpHelpers'
 import { type Controller, type HttpRequest, type HttpResponse } from '../../protocols'
+import { type EmailValidator } from '../sign_up/SignUpProtocols'
 
 export class SignInController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['email', 'password']
 
@@ -11,6 +14,10 @@ export class SignInController implements Controller {
         return HttpHelpers.badRequest(new MissingParamError(field))
       }
     }
+
+    const { email } = httpRequest.body
+
+    this.emailValidator.isValid(email)
 
     return {
       body: '',
