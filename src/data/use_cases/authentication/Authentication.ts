ad6358aae38b5b16969d4068b6ae1cb568b75d2d
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-27 12:16:34 -0300

    feat: ensure AuthenticationUseCase calls HashCompare with correct values

--- src/data/use_cases/authentication/Authentication.ts
+++ src/data/use_cases/authentication/Authentication.ts
@@ -1,11 +1,20 @@
 import { type Authentication, type AuthenticationModel } from '../../../domain/use_cases/Authentication'
+import { type HashCompare } from '../../protocols/criptography/HashCompare'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/LoadAccountByEmailRepository'
 
 export class AuthenticationUseCase implements Authentication {
-  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashCompare: HashCompare
+  ) {}
 
   async execute(authenticationModel: AuthenticationModel): Promise<string | null> {
-    await this.loadAccountByEmailRepository.load(authenticationModel.email)
+    const account = await this.loadAccountByEmailRepository.load(authenticationModel.email)
+
+    if (account) {
+      await this.hashCompare.compare(authenticationModel.password, account.password)
+      return null
+    }
 
     return null
   }
