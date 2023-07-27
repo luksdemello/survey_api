6868c6c0453db663716e095771f621f4f8aa306c
Lucas de mello <lucas_mello6@hotmail.com>
2023-07-27 12:28:39 -0300

    feat: ensure AuthenticationUseCase calls TokenGenerator with correct id

--- src/data/use_cases/authentication/Authentication.ts
+++ src/data/use_cases/authentication/Authentication.ts
@@ -1,20 +1,28 @@
 import { type Authentication, type AuthenticationModel } from '../../../domain/use_cases/Authentication'
 import { type HashCompare } from '../../protocols/criptography/HashCompare'
+import { type TokenGenerator } from '../../protocols/criptography/TokenGenerator'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/LoadAccountByEmailRepository'
 
 export class AuthenticationUseCase implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashCompare: HashCompare
+    private readonly hashCompare: HashCompare,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async execute(authenticationModel: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authenticationModel.email)
 
-    if (account) {
-      await this.hashCompare.compare(authenticationModel.password, account.password)
+    if (!account) {
       return null
     }
+
+    const passwordMath = await this.hashCompare.compare(authenticationModel.password, account.password)
+    if (!passwordMath) {
+      return null
+    }
+
+    await this.tokenGenerator.generate(account.id)
 
     return null
   }
