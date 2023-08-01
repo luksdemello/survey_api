705c9f070239c60fceecf07016c40b6eaca278df
Lucas de mello <lucas_mello6@hotmail.com>
2023-08-01 09:49:18 -0300

    test: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/infra/adapters/cryptography/BcryptAdapter.spec.ts
+++ src/infra/adapters/cryptography/BcryptAdapter.spec.ts
@@ -1,9 +1,8 @@
-import { type Hasher } from '../../../data/protocols/criptography/Hasher'
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './BcryptAdapter'
 
 interface SutTypes {
-  sut: Hasher
+  sut: BcryptAdapter
   salt: number
 }
 
@@ -19,11 +18,15 @@ const makeSut = (): SutTypes => {
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
     return new Promise(resolve => { resolve('hash') })
+  },
+
+  async compare(): Promise<string> {
+    return new Promise(resolve => { resolve('hash') })
   }
 }))
 
 describe('Bcrypt Adapter', () => {
-  it('should call bcrypt with correct value', async () => {
+  it('should call hash with correct value', async () => {
     const { sut, salt } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
@@ -31,7 +34,7 @@ describe('Bcrypt Adapter', () => {
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  it('should throw if bcrypt throws', async () => {
+  it('should throw if hash throws', async () => {
     const { sut } = makeSut()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => new Promise((resolve, reject) => { reject(new Error()) }))
 
@@ -40,10 +43,18 @@ describe('Bcrypt Adapter', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  it('should return a hash on success', async () => {
+  it('should return a valid hash on hash success', async () => {
     const { sut } = makeSut()
     const hash = await sut.hash('any_value')
 
     expect(hash).toBe('hash')
+  })
+
+  it('should call compare with correct value', async () => {
+    const { sut } = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
 })
