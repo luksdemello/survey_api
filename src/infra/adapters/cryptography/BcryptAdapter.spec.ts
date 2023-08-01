23caf0e4d71bcfc3f7f71b882afcbf64ed81f87c
Lucas de mello <lucas_mello6@hotmail.com>
2023-08-01 09:51:18 -0300

    test: ensure BcryptAdapter returns true when compare success

--- src/infra/adapters/cryptography/BcryptAdapter.spec.ts
+++ src/infra/adapters/cryptography/BcryptAdapter.spec.ts
@@ -57,4 +57,11 @@ describe('Bcrypt Adapter', () => {
 
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  it('should return true when compare success', async () => {
+    const { sut } = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+
+    expect(isValid).toBe(true)
+  })
 })
