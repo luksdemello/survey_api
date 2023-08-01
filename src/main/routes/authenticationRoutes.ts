d1b01e3064d5ef09d3ceff208625f778c19f9b0f
Lucas de mello <lucas_mello6@hotmail.com>
2023-08-01 18:30:13 -0300

    feat: add signin route

--- src/main/routes/authenticationRoutes.ts
+++ src/main/routes/authenticationRoutes.ts
@@ -1,7 +1,9 @@
 import { type Router } from 'express'
 import { makeSignUpController } from '../factories/sign-up/signUpFactory'
 import { adapterRoute } from '../adapters/express/expressRouteAdapter'
+import { makeSignInController } from '../factories/sign-in/signInFactory'
 
 export default function (router: Router): void {
   router.post('/signup', adapterRoute(makeSignUpController()))
+  router.post('/signin', adapterRoute(makeSignInController()))
 }
